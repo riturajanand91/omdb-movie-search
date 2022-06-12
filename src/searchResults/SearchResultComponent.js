e9c10605196351d70179fa3c5ac015039acdf62f
Rituraj Anand <riturajanand91@gmail.com>
2022-06-12 09:52:24 +0530

    added imdbid as keys

--- src/searchResults/SearchResultComponent.js
+++ src/searchResults/SearchResultComponent.js
@@ -12,26 +12,17 @@ import Grid from '@mui/material/Grid';
 
 
 const SearchResultComponent = (props) => {
-   
+
     return (
         <Container sx={{ mt: 3 }}>
             <Grid container spacing={2}>
                 {props.result.map(movie => (
-                    <Grid item key={movie.Year + Math.random()} xs={12} md={6} lg={4} >
+                    <Grid item key={movie.imdbID} xs={12} md={6} lg={4} >
                         <Card sx={{ maxWidth: 345 }}>
-                            <CardHeader
-                                avatar={
-                                    <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                                        R
-                                    </Avatar>
-                                }
-                                action={
-                                    <IconButton aria-label="settings">
-                                    </IconButton>
-                                }
-                                title={movie.Title}
+                            {/* <CardHeader
+                                // title={movie.Title}
                                 subheader={movie.Year}
-                            />
+                            /> */}
                             <CardMedia
                                 component="img"
                                 height="194"
@@ -40,9 +31,7 @@ const SearchResultComponent = (props) => {
                             />
                             <CardContent>
                                 <Typography variant="body2" color="text.secondary">
-                                    This impressive paella is a perfect party dish and a fun meal to cook
-                                    together with your guests. Add 1 cup of frozen peas along with the mussels,
-                                    if you like.
+                                    {movie.Title} / {movie.Year}
                                 </Typography>
                             </CardContent>
                         </Card>
